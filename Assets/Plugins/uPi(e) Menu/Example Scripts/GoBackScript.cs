f430745c378f427cad29add36e890793fd801a24
Rosco <rossfarrell7@gmail.com>
2017-05-31 16:22:42 +0800

    Yeah I have no clue what Im doing
    weeeee

---
+++ Assets/Plugins/uPi(e) Menu/Example Scripts/GoBackScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoBackScript : MonoBehaviour
+{
+    private KeyCode backKey = KeyCode.Escape;
+    private KeyCode backButton = KeyCode.Joystick1Button1;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(backKey) || Input.GetKeyDown(backButton))
+        {
+            Application.LoadLevel("StarterScene");
+        }
+    }
+}
